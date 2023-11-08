3090411f674f7047dcb77d54329797d2e3fa0b69
Kijedi <wkijedi@gmail.com>
2023-11-08 16:47:11 +0300

    Initial

--- app/src/main/java/com/langatt/pinkhearts/Screens/Profile.kt
+++ app/src/main/java/com/langatt/pinkhearts/Screens/Profile.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -15,9 +18,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.material3.DateRangePicker
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.rememberDateRangePickerState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 
@@ -31,7 +36,7 @@ fun ProfilePage(screen: String) {
     val state = rememberDateRangePickerState()
 
     Column {
-        Text(text = "Profile page", style = MaterialTheme.typography.titleLarge)
+        Text(text = "$screen Page", style = MaterialTheme.typography.titleLarge)
         Column {
             OutlinedTextField(
                 value = text,
@@ -39,17 +44,17 @@ fun ProfilePage(screen: String) {
                 label = { Text("Name") },
                 singleLine = true
             )
-            Button(
+            ElevatedButton(
                 modifier = Modifier.padding(vertical = 20.dp),
+                elevation = ButtonDefaults.elevatedButtonElevation(12.dp),
                 onClick = { showDatePicker = !showDatePicker },
                 shape = RoundedCornerShape(8.dp)
             ) {
-                Text(text = "Pick Date")
+                Text(text = "Pick Date", style = MaterialTheme.typography.bodyLarge)
             }
             if(showDatePicker){
                 DateRangePicker(state = state)
             }
-
         }
     }
 }
